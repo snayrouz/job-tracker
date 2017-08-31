43676ce711b21841fcec2c22a4e86b453c45f4cc
snayrouz <samuel.nayrouz@gmail.com>
2017-08-31 16:58:58 -0600

    Add category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,20 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe 'validations' do
-    context 'invalid attributes' do
-      it 'is invalid without a title' do
-        category = Category.new
-
-        expect(category).to be_invalid
-      end
-    end
-  end
-
-  describe 'relationships' do
-    it 'has many jobs' do
-      category = create(:category)
-
-      expect(category).to respond_to(:jobs)
-    end
+    it {is_expected.to validate_presence_of(:title)}
+    it {is_expected.to validate_uniqueness_of(:title)}
   end
 end
