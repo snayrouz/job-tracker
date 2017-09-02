e3aa4bfd5249c93938b979d8603d1cae37bbe3c4
snayrouz <samuel.nayrouz@gmail.com>
2017-09-02 12:45:36 -0600

    Comment feature test skeleton

---
+++ spec/features/comments/user_enters_comment_for_job_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "User views a job" do
+  scenario "and they enter a comment for that job" do
+
+
+
+  end
+
+  scenario "they see all comments for that specific job" do
+
+
+    
+  end
+end
