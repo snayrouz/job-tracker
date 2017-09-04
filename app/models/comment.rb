5b298663756b1c71406acfa57e2196e0b082081a
snayrouz <samuel.nayrouz@gmail.com>
2017-09-03 18:29:13 -0600

    Last commit for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
-  validates :content, presence: true
+  validates_presence_of :content
+
   belongs_to :job
 end
--- spec/features/comments/user_enters_comment_for_job_spec.rb
+++ spec/features/comments/user_enters_comment_for_job_spec.rb
@@ -2,14 +2,20 @@ require 'rails_helper'
 
 RSpec.feature "User views a job" do
   scenario "and they enter a comment for that job" do
+    company = create(:company)
+    job = create(:job)
+    body = "This job does not feel like the one for me."
+    body_2 = "Actually this job is great for the family."
 
+    visit company_job_path(company, job)
+    fill_in "comment[content]", with: body
+    click_on "Create Comment"
 
+    fill_in "comment[content]", with: body_2
+    click_on "Create Comment"
 
-  end
-
-  scenario "they see all comments for that specific job" do
-
-
-    
+    expect(page).to have_content body
+    expect(page).to have_content body_2
+    expect(body_2).to appear_before(body)
   end
 end
